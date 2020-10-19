6d6577d48422f6001e0e01859f00f00fa30c84b3
Yizhou Sheng <yizhoush@usc.edu>
2020-10-19 03:34:04 -0700

    Prototype optim gradient descent in pagmo

--- test/test_pagmo_estimation.cpp
+++ test/test_pagmo_estimation.cpp
@@ -7,8 +7,29 @@
 #include <pagmo/problem.hpp>
 
 #include "estimation_utils.hpp"
+#include "utils/optim_gd.hpp"
 
 constexpr double kEpsilon = 1e-5;
+
+TEST(PagmoEstimation, GradientDescent) {
+  pagmo::problem prob(create_problem<tds::DIFF_CERES>());
+  pagmo::algorithm algo{tds::optim_gd()};
+  pagmo::archipelago archi{1u, algo, prob, 1u}; // Proof of concept
+  archi.evolve(10);
+  archi.wait_check();
+  int i = 0;
+  for (const auto& island : archi) {
+    auto params = island.get_population().champion_x();
+    double f = island.get_population().champion_f()[0];
+    std::cout << "Best parameters from island " << (i++) << ": ";
+    for (std::size_t j = 0; j < 2; ++j) {
+      std::cout << params[j] << " ";
+      EXPECT_NEAR(params[j], true_link_lengths[j], kEpsilon);
+    }
+    std::cout << "\terror: " << f << std::endl;
+  }
+  
+}
 
 TEST(PagmoEstimation, Sade) {
   pagmo::problem prob(create_problem<tds::DIFF_NUMERICAL>());
