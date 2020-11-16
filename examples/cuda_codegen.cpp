26949e5b70fe03370f37103b6dbaea6f14e131c4
Eric Heiden <eric-heiden@outlook.com>
2020-11-15 22:16:35 -0800

    Pendulum demo for CUDA code generation

--- examples/cuda_codegen.cpp
+++ examples/cuda_codegen.cpp
@@ -6,6 +6,7 @@
 #include "math/tiny/tiny_algebra.hpp"
 #include "urdf/urdf_cache.hpp"
 #include "utils/stopwatch.hpp"
+#include "visualizer/opengl/visualizer.h"
 // clang-format on
 
 // function to be converted to CUDA code
@@ -38,8 +39,11 @@ struct ContactSimulation {
   int num_timesteps{1};
   Scalar dt{Algebra::from_double(3e-3)};
 
-  int input_dim() const { return system->dof(); }
-  int output_dim() const { return num_timesteps * system->dof(); }
+  int input_dim() const { return system->dof() + system->dof_qd(); }
+  int state_dim() const {
+    return system->dof() + system->dof_qd() + system->size() * 3;
+  }
+  int output_dim() const { return num_timesteps * state_dim(); }
 
   ContactSimulation() {
     std::string plane_filename, urdf_filename;
@@ -56,14 +60,26 @@ struct ContactSimulation {
     for (int i = 0; i < system->dof(); ++i) {
       system->q(i) = v[i];
     }
+    for (int i = 0; i < system->dof_qd(); ++i) {
+      system->qd(i) = v[i + system->dof()];
+    }
     std::vector<Scalar> result(output_dim());
     for (int t = 0; t < num_timesteps; ++t) {
       tds::forward_dynamics(*system, world.get_gravity());
       system->clear_forces();
       world.step(dt);
       tds::integrate_euler(*system, dt);
-      for (int i = 0; i < system->dof(); ++i) {
-        result[t * system->dof() + i] = system->q(i);
+      int j = 0;
+      for (int i = 0; i < system->dof(); ++i, ++j) {
+        result[j] = system->q(i);
+      }
+      for (int i = 0; i < system->dof_qd(); ++i, ++j) {
+        result[j] = system->qd(i);
+      }
+      for (const auto link : *system) {
+        result[j++] = link.X_world.translation[0];
+        result[j++] = link.X_world.translation[1];
+        result[j++] = link.X_world.translation[2];
       }
     }
     return result;
@@ -78,14 +94,14 @@ int main(int argc, char* argv[]) {
   using DiffAlgebra =
       tds::default_diff_algebra<tds::DIFF_CPPAD_CODEGEN_AUTO, 0, Scalar>::type;
 
-  ContactSimulation<DiffAlgebra> fun;
+  ContactSimulation<DiffAlgebra> simulation;
 
   // trace function with all zeros as input
-  std::vector<Dual> ax(fun.input_dim(), Dual(0));
+  std::vector<Dual> ax(simulation.input_dim(), Dual(0));
   CppAD::Independent(ax);
-  std::vector<Dual> ay(fun.output_dim());
+  std::vector<Dual> ay(simulation.output_dim());
   std::cout << "Tracing function for code generation...\n";
-  ay = fun(ax);
+  ay = simulation(ax);
   CppAD::ADFun<CGScalar> tape;
   tape.Dependent(ax, ay);
   tape.optimize();
@@ -98,7 +114,7 @@ int main(int argc, char* argv[]) {
 
   tds::CudaLibraryProcessor p(&cgen);
   p.generate_code();
-  // p.create_library();
+  p.create_library();
 
   // CppAD::cg::ModelLibraryCSourceGen<Scalar> libcgen(cgen);
   // libcgen.setVerbose(true);
@@ -114,27 +130,88 @@ int main(int argc, char* argv[]) {
   tds::CudaModel<Scalar> model(model_name);
 
   // how many threads to run on the GPU
-  int num_total_threads = 5000;
+  int num_total_threads = 400;
 
   std::vector<std::vector<Scalar>> outputs(
-      num_total_threads, std::vector<Scalar>(fun.output_dim()));
-  // generate some fake inputs
+      num_total_threads, std::vector<Scalar>(simulation.output_dim()));
+
   std::vector<std::vector<Scalar>> inputs(num_total_threads);
-  for (int i = 0; i < num_total_threads; ++i) {
-    inputs[i] = std::vector<Scalar>(fun.input_dim());
-    for (int j = 0; j < fun.input_dim(); ++j) {
-      inputs[i][j] = std::rand() * 1. / RAND_MAX;
-    }
+
+  TinyOpenGL3App app("CUDA Pendulum", 1024, 768);
+  app.m_renderer->init();
+  app.set_up_axis(2);
+  app.m_renderer->get_active_camera()->set_camera_distance(4);
+  app.m_renderer->get_active_camera()->set_camera_pitch(-30);
+  app.m_renderer->get_active_camera()->set_camera_target_position(0, 0, 0);
+  // install ffmpeg in path and uncomment, to enable video recording
+  // app.dump_frames_to_video("test.mp4");
+
+  int sphere_shape = app.register_graphics_unit_sphere_shape(SPHERE_LOD_LOW);
+  typedef tds::Conversion<DiffAlgebra, tds::TinyAlgebraf> Conversion;
+  std::vector<int> visuals;
+  for (int i = 0; i < num_total_threads * simulation.system->size(); ++i) {
+    TinyVector3f pos(0, 0, 0);
+    TinyQuaternionf orn(0, 0, 0, 1);
+    TinyVector3f color(0.5, 0.6, 1);
+    TinyVector3f scaling(0.1, 0.1, 0.1);
+    int instance = app.m_renderer->register_graphics_instance(
+        sphere_shape, pos, orn, color, scaling);
+    visuals.push_back(instance);
   }
 
   model.forward_zero.allocate(num_total_threads);
 
-  // call GPU kernel
-  for (int i = 0; i < 100; ++i) {
-    timer.start();
-    model.forward_zero(&outputs, inputs, 64);
-    timer.stop();
-    std::cout << "Kernel execution took " << timer.elapsed() << " seconds.\n";
+  TinyVector3f line_color(0.3, 0.3, 0.3);
+  float line_width = 1;
+  const int link_pos_id_offset =
+      simulation.system->dof() + simulation.system->dof_qd();
+  const int square_id = (int)std::sqrt((double)num_total_threads);
+  const float radius = 1.f;
+  for (int run = 0; run < 40; ++run) {
+    for (int i = 0; i < num_total_threads; ++i) {
+      inputs[i] = std::vector<Scalar>(simulation.input_dim(), Scalar(0));
+      inputs[i][0] = std::rand() * 1. / RAND_MAX;
+    }
+    for (int t = 0; t < 1000; ++t) {
+      timer.start();
+      // call GPU kernel
+      model.forward_zero(&outputs, inputs, 64);
+      timer.stop();
+      std::cout << "Kernel execution took " << timer.elapsed() << " seconds.\n";
+
+      for (int i = 0; i < num_total_threads; ++i) {
+        for (int j = 0; j < simulation.input_dim(); ++j) {
+          inputs[i][j] = outputs[i][j];
+        }
+      }
+
+      int visual_id = 0;
+      for (int i = 0; i < num_total_threads; ++i) {
+        TinyVector3f prev_pos(0, 0, 0);
+        for (std::size_t l = 0; l < simulation.system->size(); ++l) {
+          int sphereId = visuals[visual_id++];
+          TinyVector3f pos;
+          pos[0] = outputs[i][link_pos_id_offset + l * 3 + 0];
+          pos[1] = outputs[i][link_pos_id_offset + l * 3 + 1];
+          pos[2] = outputs[i][link_pos_id_offset + l * 3 + 2];
+          pos[0] += radius * (i % square_id) - square_id * radius / 2;
+          pos[1] += radius * (i / square_id) - square_id * radius / 2;
+          TinyQuaternionf orn(0, 0, 0, 1);
+          if (l > 0) {
+            app.m_renderer->draw_line(prev_pos, pos, line_color, line_width);
+          }
+          prev_pos = pos;
+          app.m_renderer->write_single_instance_transform_to_cpu(pos, orn,
+                                                                 sphereId);
+        }
+      }
+      DrawGridData data;
+      data.upAxis = 2;
+      app.draw_grid(data);
+      app.m_renderer->render_scene();
+      app.m_renderer->write_transforms();
+      app.swap_buffer();
+    }
   }
 
   model.forward_zero.deallocate();
